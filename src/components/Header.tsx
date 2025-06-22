b9259c21d06e01daf55b2968083953dfe873b8a4
Martin Blaščák <martin.blascak86@gmail.com>
2025-06-22 19:04:40 +0200

    added logout function to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,19 @@
+'use client'
+
 import Link from 'next/link'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 import Icon from '@/components/Icons'
 
 export default function Header() {
+	const router = useRouter()
+
+  const handleLogout = () => {
+    localStorage.removeItem('isLoggedIn')
+    localStorage.removeItem('userEmail')
+    router.push('/views/Login')
+  }
+
   return (
     <header className="w-full p-4">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
@@ -18,6 +29,9 @@ export default function Header() {
         <nav className="flex gap-4">
           <Link href="/views/Products" className="hover:underline">Produkty</Link>
 					<div className='icons_wrapper flex gap-4 items-center'>
+						<div onClick={handleLogout} className='flex items-start cursor-pointer'>
+							<Icon name="logout" width={25} color={'#010101'} className="text-red-500" />
+						</div>
 						<div className='flex items-start cursor-pointer'>
 							<Link href="/views/Login">
 								<Icon name="user" width={22} color={'#010101'} className="text-red-500" />
--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,5 +1,5 @@
 type IconProps = {
-  name: 'cart' | 'user' | 'heart'
+  name: 'cart' | 'user' | 'heart' | 'logout'
   className?: string
   width?: number | string
   height?: number | string
@@ -64,6 +64,24 @@ export default function Icon({ name, className = '', width = '1.5rem', height = 
           <path d="M12 21.35l-1.45-1.32C5.4 15.36..." />
         </svg>
       )
+
+		case 'logout'	:
+			return (
+				<svg
+          className={className}
+          width={width}
+          height={height}
+					viewBox="0 0 512 512"
+          fill={color}
+          xmlns="http://www.w3.org/2000/svg"
+        >
+					<path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 
+						224 192 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 
+						0l128-128zM160 96c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 32C43 32 0 75 0 128L0 384c0 53 43 96 96 96l64 0c17.7 0 
+						32-14.3 32-32s-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l64 0z"
+					/>
+				</svg>
+			)
 
     default:
       return <span className={className} />
