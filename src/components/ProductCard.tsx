d1c32be5060cba3f63c993c2bce07145c61db1dd
Martin Blaščák <martin.blascak86@gmail.com>
2025-06-23 08:12:44 +0200

    added star rating

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,8 +1,14 @@
+import StarRating from "@/components/StarRating"
+
 type Product = {
   id: number
   title: string
   price: number
   image: string
+	rating: {
+		rate: number
+		count: number
+	}
 }
 
 export default function ProductCard({ product }: { product: Product }) {
@@ -14,6 +20,7 @@ export default function ProductCard({ product }: { product: Product }) {
         className="w-full h-40 object-contain mb-2"
       />
       <h2 className="font-semibold text-lg">{product.title}</h2>
+			<StarRating rating={product.rating.rate} className="my-2" />
       <p className="text-sm text-gray-600">${product.price}</p>
     </div>
   )
